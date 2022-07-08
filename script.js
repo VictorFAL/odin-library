4993c6c9a22130a0491eddd14d3fb61b8064670f
Victor | Harry <victorfalmeida12@gmail.com>
2022-07-08 13:31:55 -0300

    form and some button functionalities

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let library = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,3 +10,47 @@ function Book(title, author, pages, read) {
 Book.prototype.info = function() {
     return `${this.title} by ${this.author} | ${this.pages} pages | read: ${this.read}`;
 }
+
+function addBook(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    library.push(book);
+}
+
+// test
+addBook('The Hobbit', 'J.R.R. Tokien', 295, false);
+
+const table = document.querySelector('table');
+
+library.forEach(book => {
+    table.innerHTML += `<tr>
+                            <td>
+                                ${book.title}
+                            </td>
+                            <td>
+                                ${book.author}
+                            </td>
+                            <td>
+                                ${book.pages}
+                            </td>
+                            <td>
+                                ${book.read}
+                            </td>
+                        </tr>`
+});
+
+const btnShow = document.getElementById('btn-show');
+const form = document.querySelector('form');
+btnShow.addEventListener('click', () => {
+    form.style.display = 'block';
+    btnShow.style.display = 'none';
+});
+
+const btnAdd = document.getElementById('btn-add');
+btnAdd.addEventListener('click', () => {
+    /*
+        ADD BOOKS HERE
+    */
+
+    btnShow.style.display = 'block';
+    form.style.display = 'none';
+});
