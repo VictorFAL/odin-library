442c24461f2fd228f281213ef332b71b021275cc
Victor | Harry <victorfalmeida12@gmail.com>
2022-07-11 11:57:12 -0300

    add book function complete

--- script.js
+++ script.js
@@ -11,33 +11,42 @@ Book.prototype.info = function() {
     return `${this.title} by ${this.author} | ${this.pages} pages | read: ${this.read}`;
 }
 
+// Add book obj to array
 function addBook(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     library.push(book);
 }
 
-// test
-addBook('The Hobbit', 'J.R.R. Tokien', 295, false);
-
+// Refresh table data
 const table = document.querySelector('table');
+function buildTable() {
+    table.innerHTML =   `<tr>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Pages</th>
+                            <th>Read</th>
+                        </tr>`;
+    
+    library.forEach(book => {
+        table.innerHTML += `<tr>
+                                <td>
+                                    ${book.title}
+                                </td>
+                                <td>
+                                    ${book.author}
+                                </td>
+                                <td>
+                                    ${book.pages}
+                                </td>
+                                <td>
+                                    ${book.read}
+                                </td>
+                            </tr>`
+    });
+}
 
-library.forEach(book => {
-    table.innerHTML += `<tr>
-                            <td>
-                                ${book.title}
-                            </td>
-                            <td>
-                                ${book.author}
-                            </td>
-                            <td>
-                                ${book.pages}
-                            </td>
-                            <td>
-                                ${book.read}
-                            </td>
-                        </tr>`
-});
 
+// Make book form appear
 const btnShow = document.getElementById('btn-show');
 const form = document.querySelector('form');
 btnShow.addEventListener('click', () => {
@@ -45,12 +54,22 @@ btnShow.addEventListener('click', () => {
     btnShow.style.display = 'none';
 });
 
+
+// Add form data to table
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('read');
+
 const btnAdd = document.getElementById('btn-add');
 btnAdd.addEventListener('click', () => {
-    /*
-        ADD BOOKS HERE
-    */
+    addBook(title.value, author.value, pages.value, read.value);
+    buildTable();
 
     btnShow.style.display = 'block';
     form.style.display = 'none';
 });
+
+
+// Show table headers
+buildTable();
